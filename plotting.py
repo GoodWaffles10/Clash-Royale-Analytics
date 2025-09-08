aa3c1d9ac63c7f022c0d6a1460167323272dfb51
GoodWaffles10 <henryyqliu@gmail.com>
2025-09-08 00:24:58 -0400

    plotting added

--- plotting.py
+++ plotting.py
@@ -2,3 +2,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def plot_trophy_history(csv_file):
+    df = pd.read_csv(csv_file)
+    df = df.dropna(how="all")
+
+    plt.plot(df["timestamp"], df["trophies"])
+    plt.xlabel("Date")
+    plt.ylabel("Trophies")
+    plt.title(f"Trophy History for {df["name"].iloc[0]}")
+
+    plt.show()
+
+plot_trophy_history("player_data/V2PV9LGUV.csv")
